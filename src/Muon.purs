3db58f903c3c5ec065f9d68d0159ee0c20e62b49
Ralph Morton <ralphmorton@gmail.com>
2023-01-14 08:09:05 +0200

    Utility

--- src/Muon.purs
+++ src/Muon.purs
@@ -11,24 +11,15 @@ module Muon (
   -- Elements
   text,
   el,
-  span,
-  p,
-  hr,
+  el',
+  span, span',
+  p, p',
+  hr, hr',
   img,
-  div,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  table,
-  thead,
-  tbody,
-  tr,
-  th,
-  td,
-  i,
+  div, div',
+  h1, h1', h2, h2', h3, h3', h4, h4', h5, h5', h6, h6',
+  table, table', thead, thead', tbody, tbody', tr, tr', th, th', td, td',
+  i, i',
   a,
   button,
   input,
@@ -134,6 +125,9 @@ foreign import el_ :: String -> Props -> Array Html -> Html
 el :: String -> Array Prop -> Array Html -> Html
 el tag px cx = el_ tag (props px) cx
 
+el' :: String -> Array Html -> Html
+el' tag = el_ tag (emptyProps_ unit)
+
 foreign import emptyProps_ :: Unit -> Props
 foreign import insertAttr_ :: String -> String -> Props -> Props
 foreign import insertHandler_ :: String -> (Event -> Effect Unit) -> Props -> Props
@@ -150,11 +144,20 @@ props = foldr insert (emptyProps_ unit)
 span :: Array Prop -> Array Html -> Html
 span = el "span"
 
+span' :: Array Html -> Html
+span' = el' "span"
+
 p :: Array Prop -> Array Html -> Html
 p = el "p"
 
+p' :: Array Html -> Html
+p' = el' "p"
+
 hr :: Array Prop -> Html
 hr = flip (el "hr") []
+
+hr' :: Html
+hr' = el' "hr" []
 
 img :: Array Prop -> Html
 img = flip (el "img") []
@@ -162,44 +165,86 @@ img = flip (el "img") []
 div :: Array Prop -> Array Html -> Html
 div = el "div"
 
+div' :: Array Html -> Html
+div' = el' "div"
+
 h1 :: Array Prop -> Array Html -> Html
 h1 = el "h1"
+
+h1' :: Array Html -> Html
+h1' = el' "h1"
 
 h2 :: Array Prop -> Array Html -> Html
 h2 = el "h2"
 
+h2' :: Array Html -> Html
+h2' = el' "h2"
+
 h3 :: Array Prop -> Array Html -> Html
 h3 = el "h3"
+
+h3' :: Array Html -> Html
+h3' = el' "h3"
 
 h4 :: Array Prop -> Array Html -> Html
 h4 = el "h4"
 
+h4' :: Array Html -> Html
+h4' = el' "h4"
+
 h5 :: Array Prop -> Array Html -> Html
 h5 = el "h5"
+
+h5' :: Array Html -> Html
+h5' = el' "h5"
 
 h6 :: Array Prop -> Array Html -> Html
 h6 = el "h6"
 
+h6' :: Array Html -> Html
+h6' = el' "h6"
+
 table :: Array Prop -> Array Html -> Html
 table = el "table"
+
+table' :: Array Html -> Html
+table' = el' "table"
 
 thead :: Array Prop -> Array Html -> Html
 thead = el "thead"
 
+thead' :: Array Html -> Html
+thead' = el' "thead"
+
 tbody :: Array Prop -> Array Html -> Html
 tbody = el "tbody"
+
+tbody' :: Array Html -> Html
+tbody' = el' "tbody"
 
 tr :: Array Prop -> Array Html -> Html
 tr = el "tr"
 
+tr' :: Array Html -> Html
+tr' = el' "tr"
+
 th :: Array Prop -> Array Html -> Html
 th = el "th"
+
+th' :: Array Html -> Html
+th' = el' "th"
 
 td :: Array Prop -> Array Html -> Html
 td = el "td"
 
+td' :: Array Html -> Html
+td' = el' "td"
+
 i :: Array Prop -> Array Html -> Html
 i = el "i"
+
+i' :: Array Html -> Html
+i' = el' "i"
 
 a :: Array Prop -> Array Html -> Html
 a = el "a"
